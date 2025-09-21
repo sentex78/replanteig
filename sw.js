67aa21d31efe9449a902477747c7ec2f4f619562
sentex78 <salva.rmira@gmail.com>
2025-09-21 08:42:54 +0200

    backup

--- sw.js
+++ sw.js
@@ -1,11 +1,22 @@
 self.addEventListener('install', e => {
   e.waitUntil(
-    caches.open('arids-v2').then(cache => cache.addAll([
-      'index.html', 'pes-a-volum.html', 'volum-a-pes.html', 'about.html', 'acer-corrugat.html',
-      'manifest.json', 'icon-192.png', 'icon-512.png'
+    caches.open('replanteig-v1').then(cache => cache.addAll([
+      'index.html',
+      'triangle.html',
+      'pendent.html',
+      'cotes.html',
+      'rasants.html',
+      'about.html',
+      'manifest.json',
+      'icon-192.png',
+      'icon-512.png',
+      'triangle.jpg'
     ]))
   );
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
